0dcb44910fffbb2329adec7d44b83f8ff1121ede
Andrea <andrea@wittrock.us>
2017-09-04 00:58:37 -0500

    Create src directory

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * Created by Andy on 8/25/2017.
  */
 
-import userRoutes from './routes/user';
-import loginRoutes from "./routes/login";
-import validators from "./validators/index";
+import userRoutes from './src/routes/user';
+import loginRoutes from "./src/routes/login";
+import validators from "./src/validators/index";
 
 export default ({
     routes: {
