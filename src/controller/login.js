ef627a42cb6a4ac2baec8704fa1d31525d03799f
Andrea <andrea@wittrock.us>
2017-09-12 01:57:38 -0500

    Add user permissions to profile

--- src/controller/login.js
+++ src/controller/login.js
@@ -4,8 +4,10 @@
 
 import validator from 'atp-validator';
 import User from "../model/user";
+import UserPermissionCompiled from "../model/user-permission-compiled";
 import {message, response} from 'atp-rest';
 import {createLoginToken} from '../util';
+import {o} from 'atp-sugar';
 
 export default (req, res, next) => {
     const userName = req.body.userName;
@@ -26,9 +28,16 @@ export default (req, res, next) => {
                 new User().getByUserName(userName)
                     .then(
                         user => {
+                            user = o(user).delete('password_hash').delete('login_token').raw;
                             const loginToken = createLoginToken(req, user);
                             res.header('Login-Token', loginToken);
-                            response.Success(req, res)(null);
+                            new UserPermissionCompiled().forUser(user.id).then(
+                                permissions => {
+                                    user.permissions = permissions;
+                                    response.Success(req, res)(user);
+                                },
+                                response.InternalServerError(req, res)
+                            );
                         },
                         response.InternalServerError(req, res)
                     )
