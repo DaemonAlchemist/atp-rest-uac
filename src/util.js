6fa543ebc12ece6c70973883c5c7ef5666e83904
Andrea <andrea@wittrock.us>
2017-09-22 03:05:24 -0500

    Implement logged-in user function

--- src/util.js
+++ src/util.js
@@ -5,7 +5,7 @@
 import jwt from 'jsonwebtoken';
 import config from 'atp-config';
 import Promise from 'promise';
-import {o} from 'atp-sugar';
+import User from "./model/user";
 
 config.setDefaults({
     auth: {
@@ -27,33 +27,41 @@ config.setDefaults({
     }
 });
 
-export const isLoggedIn = request => new Promise((resolve, reject) => {
+export const getLoginToken = request => {
     const allowedIn = config.get('auth.login.token.allowedIn');
     const check = (allowed, value) => allowed && typeof value !== 'undefined' ? value : false;
-    parseLoginToken(
-        check(allowedIn.header, request.headers.logintoken) ||
-        check(allowedIn.cookie, request.cookie.loginToken ) ||
-        check(allowedIn.body,   request.body.loginToken   ) ||
-        check(allowedIn.query,  request.query.loginToken  ),
-    request).then(
-        resolve,
-        () => {reject();}
-    );
+    return check(allowedIn.header, request.headers['login-token']) ||
+           check(allowedIn.cookie, request.cookie.loginToken ) ||
+           check(allowedIn.body,   request.body.loginToken   ) ||
+           check(allowedIn.query,  request.query.loginToken  );
+};
+
+export const isLoggedIn = request => new Promise((resolve, reject) => {
+    parseLoginToken(getLoginToken(request), request).then(resolve, () => {reject();});
 });
 
-export const loggedInUser = request => {
-    //TODO:  implement
-    return {id: 123};
-};
+export const loggedInUser = request => new Promise((resolve, reject) => {
+    parseLoginToken(getLoginToken(request), request).then(
+        payload => {
+            new User().getById(payload.jti).then(
+                user => {
+                    resolve(user);
+                },
+                err => {
+                    reject(err);
+                }
+            );
+        }
+    );
+});
 
 export const createLoginToken = (req, user) => jwt.sign({}, config.get('auth.login.token.secretKey'), {
     algorithm: config.get('auth.login.token.algorithm'),
     expiresIn: config.get('auth.login.token.expiresIn'),
-    notBefore: Date.now(),
+    notBefore: 0,
     audience: config.get('auth.login.token.audience')(req),
     issuer: config.get('auth.login.token.issuer')(req),
     jwtid: "" + user.id, //type coercion because jsonwebtoken module requires id to be a string
-    subject: user.username
 });
 
 export const parseLoginToken = (token, req) => new Promise((resolve, reject) => {
