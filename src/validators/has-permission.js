e7942dda2ccc2a52d466175498dabcebc2b3578e
Andrea <andrea@wittrock.us>
2017-09-22 03:05:51 -0500

    Fix hasPermission validator

--- src/validators/has-permission.js
+++ src/validators/has-permission.js
@@ -6,9 +6,15 @@ import {validate} from "atp-validator";
 import UserPermissionCompiled from "../model/user-permission-compiled";
 import {loggedInUser} from "../util";
 
-export default permissionName => validate(
+export default (permissionName, request) => validate(
     (resolve, reject) => {
-        new UserPermissionCompiled().forUser(loggedInUser().id).includes(permissionName) ? resolve() : reject();
+        loggedInUser(request).then(user => {
+            new UserPermissionCompiled()
+                .forUser(user.id)
+                .then(permissions => {
+                    permissions.includes(permissionName) ? resolve() : reject();
+                });
+        }, () => reject());
     },
     "You do not have permission to access this",
     403
