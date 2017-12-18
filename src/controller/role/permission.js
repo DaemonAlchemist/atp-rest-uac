366f6b8ed7c6416b03ce4a24f6774b1201d77fc8
Andrew Wittrock <andy@wittrock.us>
2017-12-17 23:22:26 -0600

    Refactor role-permissions and user-role controllers to use standard many2many controller

--- src/controller/role/permission.js
+++ src/controller/role/permission.js
@@ -1,46 +1,11 @@
-/**
- * Created by Andrea on 9/30/2017.
- */
 
-import {basicController, NOT_IMPLEMENTED} from "atp-rest";
+import {basicController} from "atp-rest";
 import RolePermission from "../../model/role-permission";
 import {RolePermissionBase} from "../../model/role-permission";
 import {createCrudPermissions} from "../../util";
 
-const permissionPermissions = createCrudPermissions('auth', 'permission');
-const rolePermissions = createCrudPermissions('auth', 'role');
-
-export default {
-    get: basicController.entity.subCollection({
-        model: RolePermission,
-        permission: permissionPermissions.view,
-        thisName: 'role',
-        otherName: 'permission'
-    }),
-    post: basicController.entity.create({
-        model: RolePermissionBase,
-        permission: rolePermissions.update,
-        validate: (v, req) => v
-            .check("role")
-                .required(req.params.roleId, "Role id")
-                .isInteger(req.params.roleId, "Role id")
-            .check("permission")
-                .required(req.body.permissionId, "Permission id")
-                .isInteger(req.body.permissionId, "Permission id")
-            .check("final").if(['role', 'permission'])
-    }),
-    ':permissionId': {
-        delete: basicController.entity.unlink({
-            model: RolePermissionBase,
-            permission: rolePermissions.update,
-            validate: (v, req) => v
-                .check("role")
-                    .required(req.params.roleId, "Role id")
-                   .isInteger(req.params.roleId, "Role id")
-                .check("permission")
-                    .required(req.params.permissionId, "Permission id")
-                    .isInteger(req.params.permissionId, "Permission id")
-                .check("final").if(['role', 'permission'])
-        })
-    }
-}
+export default basicController.entity.many2many(
+    'role', createCrudPermissions('auth', 'role'),
+    'permission', createCrudPermissions('auth', 'permission'),
+    RolePermission, RolePermissionBase
+);
--- src/controller/user/role.js
+++ src/controller/user/role.js
@@ -2,45 +2,15 @@
  * Created by Andrea on 9/29/2017.
  */
 
-import {basicController, NOT_IMPLEMENTED} from 'atp-rest';
+import {basicController} from 'atp-rest';
 import UserRole from "../../model/user-role";
 import {UserRoleBase} from "../../model/user-role";
 
 import {createCrudPermissions} from "../../util";
 
-const rolePermissions = createCrudPermissions('auth', 'role');
-const userPermissions = createCrudPermissions('auth', 'user');
-export default {
-    get: basicController.entity.subCollection({
-        model: UserRole,
-        permission: rolePermissions.view,
-        thisName: 'user',
-        otherName: 'role',
-    }),
-    post: basicController.entity.create({
-        model: UserRoleBase,
-        permission: userPermissions.update,
-        validate: (v, req) => v
-            .check("role")
-                .required(req.params.roleId, "Role id")
-                .isInteger(req.params.roleId, "Role id")
-            .check("user")
-                .required(req.body.userId, "User id")
-                .isInteger(req.body.userId, "User id")
-            .check("final").if(['role', 'user'])
-    }),
-    ':roleId': {
-        delete: basicController.entity.unlink({
-            model: UserRoleBase,
-            permission: userPermissions.update,
-            validate: (v, req) => v
-                .check("role")
-                    .required(req.params.roleId, "Role id")
-                    .isInteger(req.params.roleId, "Role id")
-                .check("user")
-                    .required(req.params.userId, "User id")
-                    .isInteger(req.params.userId, "User id")
-                .check("final").if(['role', 'user'])
-        })
-    }
-};
+export default basicController.entity.many2many(
+    'user', createCrudPermissions('auth', 'user'),
+    'role', createCrudPermissions('auth', 'role'),
+    UserRole, UserRoleBase
+);
+
